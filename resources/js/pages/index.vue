3dd686f7532ec97a14d8274e740bb4f684bbd774
fuureya <agiljibrin009@gmail.com>
2025-05-19 11:59:45 +0800

    first init

---
+++ resources/js/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Hello from Vue + Inertia (no TS)</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'index',
+}
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
